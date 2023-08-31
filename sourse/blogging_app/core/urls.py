196a3b6cf24c5d4ccc2604dd1c02f4487c687db7
Anastasiia323 <anastasiya.nancy@gmail.com>
2023-08-31 15:48:42 +0300

    default add files, urls

---
+++ sourse/blogging_app/core/urls.py
@@ -0,0 +1,56 @@
+from django.urls import path
+from .presentation.views.twits import (
+    add_twit,
+    twit_list,
+    tags_controller,
+    get_twit_controller,
+    delete_twit_controller,
+    edit_twit_controller
+)
+from .presentation.views.account import (
+    account_controller,
+    account_edit_controller,
+    account_add_controller,
+    change_email_controller,
+    change_email_confirm_controller,
+    change_photo_controller
+)
+from .presentation.views.login import login_user
+from .presentation.views.notifications import notifications_controller
+from .presentation.views.add_user import (
+    confirmation_user,
+    registrate_user,
+    email_notification_controller,
+    registration_confirm_controller
+)
+from .presentation.views.trends import trends_in_your_country_controller
+from .presentation.views.home import home_controller
+from .presentation.views.logout import logout_controller
+from .presentation.views.account import change_email_confirm_controller
+from .presentation.views.comments import create_comment_controller
+
+
+urlpatterns = [
+    path('', home_controller, name='home'),
+    path('tags/', tags_controller, name='tags'),
+    path('post/add/', add_twit, name='add-twit'),
+    path('twit/<int:twit_id>/', get_twit_controller, name='twit'),
+    path('<int:twit_id>/delete', delete_twit_controller, name='delete-twit'),
+    path('<int:twit_id>/edit', edit_twit_controller, name='edit-twit'),
+    path('<int:twit_id>/comment', create_comment_controller, name='create-comment'),
+    path('trends/', trends_in_your_country_controller, name='trends'),
+    path('account/', account_controller, name='account'),
+    path('account/add/', account_add_controller, name='account-add'),
+    path('account/edit', account_edit_controller, name='account-edit'),
+    path('account/edit/change_email/', change_email_controller, name='change-email'),
+    path('change_email_confirmation/', change_email_confirm_controller, name='confirm-email'),
+    path('change_photo/', change_photo_controller, name='change-photo'),
+    path('notifications/', notifications_controller, name='notifications'),
+    path('signup/', registrate_user, name='signup'),
+    path('login/', login_user, name='login'),
+    path('confirm/', registration_confirm_controller, name='confirm'),
+    path('notification/', email_notification_controller, name='message'),
+    path('logout/', logout_controller, name='logout'),
+    path('account/add_twit', add_twit, name='add-twit')
+]
+
